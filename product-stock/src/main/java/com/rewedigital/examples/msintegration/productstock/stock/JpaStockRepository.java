ef5419dbca65a540c360d17fa69b7d7ac5545b69
Carsten Zeitz <>
2019-09-29 12:59:00 +0200

    Prepared inital workshop version

--- product-stock/src/main/java/com/rewedigital/examples/msintegration/productstock/stock/JpaStockRepository.java
+++
@@ -1,6 +0,0 @@
-package com.rewedigital.examples.msintegration.productstock.stock;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JpaStockRepository extends JpaRepository<Stock, String> {
-}
--- product-stock/src/main/java/com/rewedigital/examples/workshop/productstock/ProductStockApplication.java
+++ product-stock/src/main/java/com/rewedigital/examples/workshop/productstock/ProductStockApplication.java
@@ -1,4 +1,4 @@
-package com.rewedigital.examples.msintegration.productstock;
+package com.rewedigital.examples.workshop.productstock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
